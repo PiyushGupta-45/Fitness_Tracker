bd3f30740721e3f47c4bfa9302f8d7e62c32ba70
PiyushGupta <pgupta3883@gmail.com>
2025-02-22 21:27:31 +0530

    BMR added

--- src/Website/BMR.jsx
+++ src/Website/BMR.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const BMR = () => {
   return (
-    <div>BMR</div>
+    <>
+    <p className='text-center py-1 bg-emerald-200 font-medium text-[20px]'>Here you can Calculate the BMR of your body</p>
+    <div className='flex my-1 border border-emerald-600'>
+      random
+    </div>
+    </>
   )
 }
 
