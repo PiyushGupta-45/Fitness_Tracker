b16a5975368139fb745d9e6bdce7089d37644cb1
PiyushGupta <pgupta3883@gmail.com>
2025-03-06 20:08:51 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import './App.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+
 import Layout from './Layout'
+
 import Home from './Website/Home'
 import About from './Website/About'
+import Goals from './Website/Goals/Goals'
 import Contact from './Website/Contact'
+import CaloriesCalculator from './Website/CaloriesCalculator'
+import BMR from './Website/BMR'
+
 import Tracking from './Website/Track_Workout/tracking'
 import Posture from './Website/check_Posture/Posture'
-import Goals from './Website/Goals/Goals'
+
 import Running from './Website/Track_Workout/Cardio/running'
 import Back from './Website/Track_Workout/Back/Back'
 import Chest from './Website/Track_Workout/Chest/Chest'
 import Biceps from './Website/Track_Workout/Arms/Biceps'
 import Legs from './Website/Track_Workout/Legs/Legs'
-import BMR from './Website/BMR'
-import CaloriesCalculator from './Website/CaloriesCalculator'
+import Shoulder from './Website/Track_Workout/Shoulder/Shoulder'
 
 function App() {
   const router = createBrowserRouter(
@@ -22,16 +27,19 @@ function App() {
         <Route path='' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
+        <Route path='/goals' element={<Goals/>}/>
+        <Route path='/bmr' element={<BMR/>}/>
+        <Route path='/calories-calculator' element={<CaloriesCalculator/>}/>
+
         <Route path='/tracking' element={<Tracking/>}/>
         <Route path='/posture' element={<Posture/>}/>
-        <Route path='/goals' element={<Goals/>}/>
+        
         <Route path='/running' element={<Running/>}/>
         <Route path='/back' element={<Back/>}/>
         <Route path='/chest' element={<Chest/>}/>
         <Route path='/arms' element={<Biceps/>}/>
         <Route path='/legs' element={<Legs/>}/>
-        <Route path='/bmr' element={<BMR/>}/>
-        <Route path='/calories-calculator' element={<CaloriesCalculator/>}/>
+        <Route path='/shoulder' element={<Shoulder/>}/>
       </Route>
     )
   )
